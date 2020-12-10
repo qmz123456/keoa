da01b6d9478eff2767b97651df162b8a34cfa2eb
qmz <3489143746@qq.com>
2020-12-10 11:06:02 +0800

    初始化keoa项目

---
+++ src/main/java/cn/rg/qmz/App.java
@@ -0,0 +1,19 @@
+package cn.rg.qmz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/mysql?useUnicode=true&characterEncoding=UTF-8";
+            Connection conn= DriverManager.getConnection(url,"root","123");
+            System.out.println(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
